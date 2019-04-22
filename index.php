db76a3b3bd11c04182ed22d0ab58f718f1c26ee4
Thinnakorn Pattha <baleethai@gmail.com>
2019-04-22 09:44:06 +0700

    updated room

--- index.php
+++ index.php
@@ -16,18 +16,23 @@
 		}
 		ul li a {
 			font-size: 24px;
+			text-decoration: none;
+			color: black;
+		}
+		ul li a.active {
+			color: green;
 		}
 	</style>
 </head>
 <body>
 	<div>
 		<ul>
-			<li><a href="?room=ROOM1">Room 1</a></li>
-			<li><a href="?room=ROOM2">Room 2</a></li>
-			<li><a href="?room=ROOM3">Room 3</a></li>
+			<li><a href="/?room=ROOM1" class="<?php echo isset($_GET['room']) && $_GET['room'] == 'ROOM1' ? 'active' : ''; ?>">Room 1</a></li>
+			<li><a href="/?room=ROOM2" class="<?php echo isset($_GET['room']) && $_GET['room'] == 'ROOM2' ? 'active' : ''; ?>">Room 2</a></li>
+			<li><a href="/?room=ROOM3" class="<?php echo isset($_GET['room']) && $_GET['room'] == 'ROOM3' ? 'active' : ''; ?>">Room 3</a></li>
 		</ul>
 	</div>
 	<div id="otEmbedContainer" style="width:800px; height:640px"></div>
-	<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=04389509-8f81-4f25-8ed4-cd904004b5f9&room=<?php echo isset($_GET['room']) ? $_GET['room'] : 'DEFAULT_ROOM'; ?>"></script>
+	<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=04389509-8f81-4f25-8ed4-cd904004b5f9&room=<?php echo isset($_GET['room']) ? $_GET['room'] : 'ROOM1'; ?>"></script>
 </body>
 </html>
